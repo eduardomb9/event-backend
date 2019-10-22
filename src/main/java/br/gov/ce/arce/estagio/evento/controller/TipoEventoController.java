32670aec3b9c1d31895229c7454ebd5ade859dd4
eduardomb9 <maranhaoeduardo9@gmail.com>
2019-10-22 15:32:54 -0300

    refatorando servico tipoevento

--- src/main/java/br/gov/ce/arce/estagio/evento/controller/TipoEventoController.java
+++ src/main/java/br/gov/ce/arce/estagio/evento/controller/TipoEventoController.java
@@ -1,26 +1,29 @@
 package br.gov.ce.arce.estagio.evento.controller;
 
-import br.gov.ce.arce.estagio.evento.repository.TipoEventoRepository;
+import br.gov.ce.arce.estagio.evento.service.TipoEventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/tipo_evento")
 public class TipoEventoController {
 
+    private TipoEventoService tipoEventoService;
+
     @Autowired
-    TipoEventoRepository tipoEventoRepository;
-
-
-    public TipoEventoController() {
+    public TipoEventoController(TipoEventoService service) {
+        this.tipoEventoService = service;
     }
 
     @CrossOrigin
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity getTipoEventos() {
-        return ResponseEntity.ok(tipoEventoRepository.findAll());
+        return ResponseEntity.ok(tipoEventoService.getTiposEventos());
     }
 
 }
